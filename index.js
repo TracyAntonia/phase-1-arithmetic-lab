0847a69188547d7203d87a6ac45a1ee948765f03
TracyAntonia <aantonjuguna@gmail.com>
2022-11-23 15:56:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+console.log(multiply)
+
+
+let random = 1
+
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+
+
+Math.max(16,17,18,19,20)
+let max = 20
